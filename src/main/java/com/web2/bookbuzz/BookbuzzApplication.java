3dafde7037bf56496a8efc64c07322eae837cee5
Maycon Douglas <mdrs.maycon@gmail.com>
2023-09-27 16:12:56 -0300

    Initial commit

---
+++ src/main/java/com/web2/bookbuzz/BookbuzzApplication.java
@@ -0,0 +1,13 @@
+package com.web2.bookbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookbuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookbuzzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web2/bookbuzz/BookbuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web2.bookbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookbuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
