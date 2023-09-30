c42a155d62d53e8c52f5ad5725160ebfb825faf6
Maycon Douglas <mdrs.maycon@gmail.com>
2023-09-30 20:45:30 -0300

    feat: Books routers & all inizialize project

--- src/main/java/com/web2/bookbuzz/BookbuzzApplication.java
+++ src/main/java/com/web2/bookbuzz/BookbuzzApplication.java
@@ -2,8 +2,10 @@ package com.web2.bookbuzz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.web2.bookbuzz.repositories")
 public class BookbuzzApplication {
 
 	public static void main(String[] args) {
