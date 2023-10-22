041547aad5a584d2875d9410735a644b7a50db7f
Ítalo de Azevedo Albuquerque <italodeazevedo99@gmail.com>
2023-10-22 19:10:08 -0300

    fix search

--- src/main/java/com/web2/bookbuzz/repositories/UserBookSituationRepository.java
+++ src/main/java/com/web2/bookbuzz/repositories/UserBookSituationRepository.java
@@ -1,11 +1,14 @@
 package com.web2.bookbuzz.repositories;
 
+import com.web2.bookbuzz.models.BookModel;
 import com.web2.bookbuzz.models.UserBookSituationModel;
 
 import jakarta.transaction.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +17,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserBookSituationRepository extends JpaRepository<UserBookSituationModel, Integer> {
+
+    List<UserBookSituationModel> findAll();
+
+    List<UserBookSituationModel> findAll(Specification<UserBookSituationModel> spec);
+
     @Query("SELECT ubsm FROM UserBookSituationModel ubsm WHERE ubsm.userId.id = :user_id AND ubsm.bookId = :book_id")
     Optional<UserBookSituationModel> findByUserIdAndBookId(@Param("user_id") int user_id,
             @Param("book_id") String book_id);
